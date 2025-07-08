0043c105c2a6b966f30db5a13afac7a255cf3664
umerrais006 <umerrais006@icloud.com>
2025-07-08 12:34:09 +0500

    initial Project Setuo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-yellow-200">Tailwind is working! 🎉</h1>
+      <p className="mt-4 text-lg text-gray-600">Welcome to your Stopwatch App</p>
+    </div>
+  );
+}
+
